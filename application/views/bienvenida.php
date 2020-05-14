d94d0b5714d8425fbe0edb21247b2838fe7bb0d9
Ruben <53225487+Rubepi@users.noreply.github.com>
2020-05-14 16:59:02 -0400

    a por o

---
+++ application/views/bienvenida.php
@@ -0,0 +1,28 @@
+
+
+
+
+<div style="width: 80%;margin: 5% 10% 10% 10%;">
+
+    <div class="negrilla azultext" style="width: 100%;font-size: 60px;">Bienvenida!!!</div>
+    <div class="negrilla alin_cen " style=" color: #999999;width: 100%;font-size: 40px;"><?php echo $this->session->userdata('nombres') . " " . $this->session->userdata('apellidos') ?></div>
+    <div class="f30 negrilla">Te damos la bienvenida al <span class="azultext f30">Sistema de Gestion OnLine STS GO!</span></div>
+
+
+    <h1>
+        <?php
+        //echo phpinfo();
+       //////// $numero = '0001234567890201811151310009990000101010000000100005';
+        // $numero2= 0001234567890201811151310009990000101010000000100005;
+       // $valor = gmp_init($numero, 10);
+
+        //$valor16 = gmp_strval($valor, 16);
+        ///////echo strtoupper($valor16) . "<br>";
+        ////////echo ($numero) . "<br>";
+
+        /////////echo "<br>D83FF05798D81FFC7426DE08E0C6F3C79884A6A5"
+        ?>
+    </h1>
+
+
+</div>
